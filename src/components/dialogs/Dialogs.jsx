80cc3c0fc4e368a05f7f37ed5b96e95ff4e86c39
arina nikoliuk <arishanikoliuk@gmail.com>
2024-07-25 17:01:05 +0200

    commit

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -2,6 +2,20 @@ import React from "react"
 import "./dialogs.css"
 import DialogsItem from "./dialogsItem/DialogsItem"
 import Messages from "./dialogsMessages/Messages"
+let dialogNames = [
+    {name:"Ivan Ivanich", id:1}, 
+    {name:"Alex Vasilivich", id:2}, 
+    {name:"Roman Romanivich", id:3}, 
+    {name:"Alexander Alexeivich", id:4},
+    {name:"Dmitriy Nikolaievich", id:5}
+]
+let messageItems = [
+    {message:"Hello, how are you?", id:1},
+    {message:"Hi, Im okey and you Alex", id:2},
+    {message:"Good morning Im cool what about you Alexander", id:3},
+    {message:"Hi Im perfect", id:4},
+    {message:"Hello everybody. Do you want to go to do sport this saturday?", id:5}
+]
 function Dialog(props){
     return(
         <div className="dialogs">
@@ -9,16 +23,18 @@ function Dialog(props){
             <p className="answer">{props.answer}</p>
             <div className="dialog">
                 <div className="items">
-                    <DialogsItem name={"Ivan Ivanich:"} id={"1"} />
-                    <DialogsItem name={"Alex Vasilivich:"} id={"2"} />
-                    <DialogsItem name={"Roman Romanivich:"} id={"3"} />
-                    <DialogsItem name={"Alexander Alexeivich:"} id={"4"} />
+                    {dialogNames.map((e)=><DialogsItem name={e.name} id={e.id} />)}
+                    {/* <DialogsItem name={dialogNames[0].name} id={dialogNames[0].id} />
+                    <DialogsItem name={dialogNames[1].name} id={dialogNames[1].id} />
+                    <DialogsItem name={dialogNames[2].name} id={dialogNames[2].id} />
+                    <DialogsItem name={dialogNames[3].name} id={dialogNames[3].id} />  */}
                 </div>
                 <div className="messages">
-                    <Messages message={"Hello, how are you?"} />
-                    <Messages message={"Hi, Im okey and you Alex"} />
-                    <Messages message={"Good morning Im cool what about you Alexander"} />
-                    <Messages message={"Hi Im perfect"} />
+                    {messageItems.map((e)=><Messages message={e.message} id={e.id} />)}
+                    {/* <Messages message={messageItems[0].message} id={dialogNames[0].id} />
+                    <Messages message={messageItems[1].message} id={dialogNames[1].id} />
+                    <Messages message={messageItems[2].message} id={dialogNames[2].id} />
+                    <Messages message={messageItems[3].message} id={dialogNames[3].id} /> */}
                 </div>
                 {/* <div className="message">
                     <Link to="/dialogs/2" className="name">Roman Romanich</Link> 
