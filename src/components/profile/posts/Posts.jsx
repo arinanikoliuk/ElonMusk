cff65195704953595184c234e9deb74f95364aea
arina nikoliuk <arishanikoliuk@gmail.com>
2024-10-16 18:00:17 +0200

    created a cicle

--- src/components/profile/posts/Posts.jsx
+++ src/components/profile/posts/Posts.jsx
@@ -6,13 +6,20 @@ import cat1 from "./post2/img/cat1.webp"
 import cat2 from "./post2/img/cat2.jpg"
 import cat3 from "./post2/img/cat3.jpeg"
 import cat4 from "./post2/img/cat4.png"
+let post1_data=[
+    {text:"Space X is the best company", id:1, likes:121}, 
+    {text:"My rockets are explaining Mars", id:2, likes:345},
+    {text:"Tesla is the best car you can have", id:3, likes:159}
+]
 function Posts() {
     return(
+
         <div className="posts">
             <h2 className="posts-text">My posts</h2>
             <input placeholder="enter the post" />
             <button>Add post</button>
-            <Post message="My name is Elon Musk and I create rockets with my company. Soon we are planning to go to expedition on a Mars" />
+            {/* <Post message="My name is Elon Musk and I create rockets with my company. Soon we are planning to go to expedition on a Mars" /> */}
+            {post1_data.map((e)=> <Post message={e.text} id={e.id} likes={e.likes}/>)}
             {/* <Dialog name="Mark Zuckerberg" />
             <Dialog name="Elon Musk" />
             <Dialog name="Bill Gates" /> */}
