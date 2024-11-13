3cb24395a16cd0592b2c1d99a6857d95a78f7c5f
arina nikoliuk <arishanikoliuk@gmail.com>
2024-11-13 17:53:10 +0100

    creting state

--- src/App.js
+++ src/App.js
@@ -30,9 +30,9 @@ function App(props) {
     <NavBar />
     {/* <Profile name={"Elon Musk"} /> */}
     <Routes>
-      <Route path="/" element={<Profile description_cats={props.description_cats}/>} />
-      <Route path="/profile" element={<Profile post1_data={props.post1_data}/>} />
-      <Route path="/dialogs" element={<Dialogs dialogs_data={props.dialogs_data} messages_data={props.messages_data}  />} />
+      <Route path="/" element={<Profile description_cats={props.state.description_cats}/>} />
+      <Route path="/profile" element={<Profile post1_data={props.state.post1_data}/>} />
+      <Route path="/dialogs" element={<Dialogs dialogs_data={props.state.dialogs_data} messages_data={props.state.messages_data}  />} />
       <Route path="/feed" element={<Feed />} />
       <Route path="/friends" element={<Friends />} />
     </Routes>
