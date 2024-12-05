3ff988aa8361809987e2ffd004c357916053679d
arina nikoliuk <arishanikoliuk@gmail.com>
2024-12-05 01:00:12 +0100

    flux architecture part 1

--- src/index.js
+++ src/index.js
@@ -5,16 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from "./Data/state";
 import {addPost} from "./Data/state"
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+let rerenderthree=()=>{
+  ReactDOM.render(
   <React.StrictMode>
     <App 
       state={state} addPost={addPost}
     />
   </React.StrictMode>
-)
+);}
+rerenderthree()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
